a394aa935e52a8b48c20efb44e51757802e55479
AbhiAtiUttam <Abhi@glocalize.net>
2024-02-23 18:04:58 +0530

    Card created , next adding animation

---
+++ src/components/Cards.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from "./cards.module.css"
+function Cards() {
+  return (
+    <>
+    <div className={style.card2}>hello world</div>
+    <div className={style.card1}>hello world</div>
+    <div className={style.card}>hello world</div>
+    </>
+  )
+}
+
+export default Cards
